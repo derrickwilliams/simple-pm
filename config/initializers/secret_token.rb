f53db22a0550400a842c5149e105bf9a4f5e716e
DerrickW <derrickawilliams@gmail.com>
2014-02-17 16:13:02 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimplePm::Application.config.secret_key_base = '00e9e27c6005c7ed58be3a08a31f67134ab7828736afcac00d8debc70e3a33e586fa7bd4c6bb89c1d61625605d0733ac7842ce37694835feae8446ae39b4c484'
